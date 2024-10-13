eeb0b2ddda266844dfdd9be5ad4e75e5061019fb
abui <adjiem31@gmail.com>
2024-10-14 02:50:16 +0700

    polish

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# python-rpc" 
+# Python RPC
+
+## Setup
+
+1. Run python server
+
+```bash
+python server.py
+```
+
+2. Run python client
+
+```bash
+python client.py
+```
--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import xmlrpc.client
 server_url = 'http://localhost:8000'
 server = xmlrpc.client.ServerProxy(server_url)
 
-batas = 11
-prima_list = server.rpc_prima(batas)
+limit = 11
+prima_list = server.rpc_prima(limit)
 
-print(f"Bilangan prima hingga {batas}: {prima_list}")
+print(f"Bilangan prima hingga {limit}: {prima_list}")
